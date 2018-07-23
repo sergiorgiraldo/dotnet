fda7bcdf5886fecda8f4f73ae6e515655cc64aa4
sergio giraldo <sergiorgiraldo@gmail.com>
2018-07-23 20:44:38 -0300

    20180723

---
+++ ConsoleApp.SQLite/Migrations/20180723233255_Authors.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp.SQLite.Migrations
+{
+    public partial class Authors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Profile = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Posts");
+        }
+    }
+}
---
+++ JuntarArquivosEleicao/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultipleFilesToSingleFile(@"C:\Users\Sergio\Downloads\votacao_partido_munzona_2016", "*.txt", @"C:\Users\Sergio\source\R-Lang\eleicoes2016\votacao.csv");
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void MultipleFilesToSingleFile(string dirPath, string filePattern, string destFile)
+        {
+            string[] arrFiles = Directory.GetFiles(dirPath, filePattern);
+
+            Console.WriteLine("Total File Count : " + arrFiles.Length);
+
+            using (TextWriter tw = new StreamWriter(destFile, true, Encoding.GetEncoding("ISO-8859-1")))
+            {
+                foreach (string filePath in arrFiles)
+                {
+                    using (TextReader tr = new StreamReader(filePath, Encoding.GetEncoding("ISO-8859-1")))
+                    {
+                        tw.WriteLine(tr.ReadToEnd());
+                        tr.Close();
+                        tr.Dispose();
+                    }
+                    Console.WriteLine("File Processed : " + filePath);
+                }
+
+                tw.Close();
+                tw.Dispose();
+            }
+        }
+    }
+
+}
