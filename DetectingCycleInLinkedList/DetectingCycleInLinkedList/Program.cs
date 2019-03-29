0b02ce4abe797982c2c0399ffec0cd28d66e9736
Sergio Giraldo at PagSeguro <sgiraldo@uolinc.com>
2019-03-29 15:57:31 -0300

    linked list

---
+++ DetectingCycleInLinkedList/DetectingCycleInLinkedList/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetectingCycleInLinkedList
+{
+    class LinkedList
+    {
+        Node Head;
+
+        public class Node
+        {
+            public int value;
+            public string key;
+            public Node NextNode;
+
+            public Node(int value)
+            {
+                key = Guid.NewGuid().ToString();
+                this.value = value;
+            }
+        }
+
+        public LinkedList(Node head)
+        {
+            Head = head;
+        }
+
+        public Boolean hasLoop() //brute force
+        {
+            List<string> lstKeys = new List<string>
+            {
+                Head.key
+            };
+
+            Node tempNode = Head.NextNode;
+            while (tempNode != null)
+            {
+                if (lstKeys.IndexOf(tempNode.key) != -1) return true;
+                lstKeys.Add(tempNode.key);
+                tempNode = tempNode.NextNode;
+            }
+            return false;
+        }
+
+        public Boolean hasLoop2() //aha
+        {
+            Node tempNode = Head;
+            Node tempNode1 = Head.NextNode;
+            while (tempNode != null && tempNode1 != null)
+            {
+                if (tempNode.Equals(tempNode1))
+                {
+                    return true;
+                }
+
+                if ((tempNode1.NextNode != null) && (tempNode.NextNode != null))
+                {
+                    tempNode1 = tempNode1.NextNode.NextNode;
+                    tempNode = tempNode.NextNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        public static void Main()
+        {
+            Node head = new Node(1);
+            LinkedList ll = new LinkedList(head);
+
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+            Node node6 = new Node(6);
+            Node node7 = new Node(7);
+
+            head.NextNode = node2;
+            node2.NextNode = node3;
+            node3.NextNode = node4;
+            node4.NextNode = node5;
+            node5.NextNode = node6;
+            node6.NextNode = node7;
+            node7.NextNode = node4;
+
+            Console.WriteLine(ll.hasLoop());
+            Console.Read();
+        }
+    }
+}
---
+++ EvalExpression/EvalExpression/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvalExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exp = "4 - 3 * 12 + 300"; //268
+            var result = calc(exp);
+        }
+
+        static int calc(string exp)
+        {
+            Stack<int> numbers = new Stack<int>();
+            Stack<string> operators = new Stack<string>();
+            List<string> precedence = new List<string> { "*", "/", "+", "-" };
+
+            var tokens = exp.Split(" ");
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numbers.Push(Int32.Parse(tokens[i]));
+                }
+                else{
+                    if (operators.Count > 0)
+                    {
+                        var currentOperator = operators.Peek();
+                        if (precedence.IndexOf(tokens[i]) > precedence.IndexOf(currentOperator))
+                        {
+                            var n1 = numbers.Pop();
+                            var n2 = numbers.Pop();
+                            currentOperator = operators.Pop();
+                            switch (currentOperator)
+                            {
+                                case "+": numbers.Push(n2 + n1); break;
+                                case "-": numbers.Push(n2 - n1); break;
+                                case "*": numbers.Push(n2 * n1); break;
+                                case "/": numbers.Push(n2 / n1); break;
+                                default: break;
+                            }
+                        }
+                    }
+                    operators.Push(tokens[i]);
+                }
+            }
+            var result = 0;
+            while (operators.Count > 0)
+            {
+                var nFinal1 = numbers.Pop();
+                var nFinal2 = numbers.Pop();
+                var finalOperator = operators.Pop();
+                switch (finalOperator)
+                {
+                    case "+": result = (nFinal2 + nFinal1); break;
+                    case "-": result = (nFinal2 - nFinal1); break;
+                    case "*": result = (nFinal2 * nFinal1); break;
+                    case "/": result = (nFinal2 / nFinal1); break;
+                }
+                numbers.Push(result);
+            }
+            return result;
+        }
+    }
+}
