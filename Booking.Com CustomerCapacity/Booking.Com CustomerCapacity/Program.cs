fcf40e9e08393e4e690e2154ef9b20a5313987ac
sergio giraldo <sergiorgiraldo@gmail.com>
2019-03-28 22:25:02 -0300

    booking.com

---
+++ Booking.Com CustomerCapacity/Booking.Com CustomerCapacity/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Com_CustomerCapacity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestRunner(Test00, 1);
+            TestRunner(Test01, 2);
+            TestRunner(Test02, 0);
+            TestRunner(Test03, 2);
+            TestRunner(Test04, 2);
+            TestRunner(Test05, 3);
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+        }
+
+        class Call
+        {
+            public Call(int start, int end)
+            {
+                Start = start;
+                End = end;
+            }
+
+            public int Start { get; set; }
+            public int End { get; set; }
+        }
+
+        static int numberOfAgentsToAdd(int executives, int[][] callTimes)
+        {
+            var set = new SortedList<int, int>();
+            for (int i = 0; i < callTimes.Length; ++i)
+            {
+                var b = callTimes[i][0];
+                var e = callTimes[i][1];
+
+                if (!set.ContainsKey(b)) set.Add(b, +1); else set[b]++;
+                if (!set.ContainsKey(e)) set.Add(e, -1); else set[e]--;
+            }
+            var max = 0;
+            var num = 0;
+            foreach (var val in set.Values)
+            {
+                num += val;
+                if (max < num) max = num;
+            }
+            return max - executives;
+
+        }
+
+        static int Test00()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[3][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222020;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222000;
+            callsTimes[1][1] = 1481222040;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222030;
+            callsTimes[2][1] = 1481222035;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static int Test01()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[3][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222020;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222001;
+            callsTimes[1][1] = 1481222040;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222002;
+            callsTimes[2][1] = 1481222035;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static int Test02()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[3][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222010;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222020;
+            callsTimes[1][1] = 1481222030;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222040;
+            callsTimes[2][1] = 1481222050;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static int Test03()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[3][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222050;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222020;
+            callsTimes[1][1] = 1481222050;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222040;
+            callsTimes[2][1] = 1481222050;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static int Test04()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[4][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222050;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222020;
+            callsTimes[1][1] = 1481222050;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222040;
+            callsTimes[2][1] = 1481222050;
+            callsTimes[3] = new int[2];
+            callsTimes[3][0] = 1481222050;
+            callsTimes[3][1] = 1481222060;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static int Test05()
+        {
+            var numOfAgent = 1;
+            var callsTimes = new int[4][];
+
+            callsTimes[0] = new int[2];
+            callsTimes[0][0] = 1481222000;
+            callsTimes[0][1] = 1481222050;
+            callsTimes[1] = new int[2];
+            callsTimes[1][0] = 1481222020;
+            callsTimes[1][1] = 1481222050;
+            callsTimes[2] = new int[2];
+            callsTimes[2][0] = 1481222040;
+            callsTimes[2][1] = 1481222050;
+            callsTimes[3] = new int[2];
+            callsTimes[3][0] = 1481222045;
+            callsTimes[3][1] = 1481222060;
+
+            return numberOfAgentsToAdd(numOfAgent, callsTimes);
+        }
+
+        static void TestRunner(Func<int> test, int expected)
+        {
+            int val = 0;
+            Console.WriteLine(
+              test.Method.Name + " = {0} -> {1}",
+              (val = test.Invoke()), val == expected ? "PASS" : "FAIL");
+        }
+
+
+    }
+}
---
+++ Booking.Com Hotels/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static List<int> sort_hotels(string keywords, List<int> hotel_ids, List<string> reviews)
+        {
+            var result = new Dictionary<int, int>();
+            for (int i = 0; i < reviews.Count; i++)
+            {
+                var total = Matches(keywords, reviews[i]);
+                if (result.ContainsKey(hotel_ids[i]))
+                {
+                    result[hotel_ids[i]] += total;
+                }
+                else
+                {
+                    result.Add(hotel_ids[i], total);
+                }
+            }
+            var sortedResult = result.OrderByDescending(key => key.Value).ToList();
+            var lstHotel = new List<int>();
+            foreach (var item in sortedResult)
+            {
+                lstHotel.Add(item.Key);
+            }
+            return lstHotel;
+        }
+
+        static int Matches(string keywords, string review)
+        {
+            var lstKeyword = keywords.Split(' ');
+            var total = 0;
+            foreach (var keyword in lstKeyword)
+            {
+                total += Regex.Matches(review, keyword, RegexOptions.IgnoreCase).Count;
+           }
+            return total;
+        }
+
+        static void Main(string[] args)
+        {
+            var k = "breakfast beach citycenter location metro view staff price";
+            var h = new List<int> { 1, 2, 1, 1, 2 };
+            var r = new List<string> {
+                "This hotel has a nice view of the citycenter. The location is perfect.",
+"The breakfast is ok. Regarding location, it is quite far from citycenter but price is cheap so it is worth.",
+"Location is excellent, 5 minutes from citycenter. There is also a metro station very close to the hotel.",
+"They said I couldn't take my dog and there were other guests with dogs! That is not fair.",
+"Very friendly staff and good cost-benefit ratio. Location is a bit far from citycenter."
+
+            };
+            sort_hotels(k, h, r);
+            Console.ReadLine();
+        }
+    }
+}
