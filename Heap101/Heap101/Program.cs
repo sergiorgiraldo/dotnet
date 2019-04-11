5c4e0058d294896785b1e3428d56f174e7d0ea62
Sergio Giraldo at PagSeguro <sgiraldo@uolinc.com>
2019-04-11 10:33:34 -0300

    data structures

---
+++ Heap101/Heap101/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+
+namespace Heap101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MinHeap mh = new MinHeap(7);
+            mh.Insert(8);
+            mh.Insert(5);
+            mh.Insert(10);
+            mh.Insert(3);
+            mh.Insert(1);
+            mh.Insert(7);
+            mh.Insert(20);
+            Console.WriteLine(mh.GetMin());
+            mh.DisplayHeap();
+
+            mh.RemoveMin();
+            Console.WriteLine(mh.GetMin());
+            mh.DisplayHeap();
+
+            mh.Remove(7);
+            Console.WriteLine(mh.GetMin());
+            mh.DisplayHeap();
+            int[] arr = { 77, 64, 21, 89, 92, 17, 30, 42, 50, 2 };
+            mh.BuildMinHeap(arr);
+            mh.DisplayHeap();
+        }
+    }
+
+    class MinHeap
+    {
+        int[] arr;
+        int arrSize;//size for the array container
+        int heapSize;//keeps track of the number of elements
+        public MinHeap()
+        {
+            arrSize = 0;
+            heapSize = 0;
+            arr = new int[arrSize];
+        }
+
+        public MinHeap(int size)
+        {
+            arr = new int[size];
+        }
+
+        public void SetHeapSize(int size)
+        {
+            arrSize = size;
+            arr = new int[size];
+        }
+
+        public void Insert(int value)
+        {
+            if (heapSize == arr.Length)
+            {
+                throw new Exception("Heap is at full capacity!");
+            }
+
+            arr[heapSize] = value;
+            heapSize++;
+            SiftUp(heapSize - 1);
+        }
+
+        public void Remove(int value)
+        {
+            for (int i = 0; i < heapSize - 1; i++)
+            {
+                if (arr[i] == value)
+                {
+                    arr[i] = arr[heapSize - 1];
+                    heapSize--;
+                    siftDown(i);
+                    break;
+                }
+            }
+        }
+
+        public void RemoveMin()
+        {
+            if (heapSize == 0)
+            {
+                throw new Exception("Heap is empty!");
+            }
+
+            arr[0] = arr[heapSize - 1];
+            heapSize--;
+            if (heapSize > 0)
+            {
+                siftDown(0);
+            }
+        }
+
+        private void SiftUp(int index)
+        {
+            int parentIndex, temp;
+            if (index != 0)
+            {
+                parentIndex = GetParentIndex(index);
+                if (arr[parentIndex] > arr[index])
+                {
+                    temp = arr[parentIndex];
+                    arr[parentIndex] = arr[index];
+                    arr[index] = temp;
+                    SiftUp(parentIndex);
+                }
+            }
+        }
+
+        private int GetParentIndex(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private void siftDown(int nodeIndex)
+        {
+            int leftChildIndex, rightChildIndex, minIndex, tmp;
+
+            leftChildIndex = GetLeftChildIndex(nodeIndex);
+
+            rightChildIndex = GetRightChildIndex(nodeIndex);
+
+            if (rightChildIndex >= heapSize)
+            {
+                if (leftChildIndex >= heapSize)
+                {
+                    return;
+                }
+
+                minIndex = leftChildIndex;
+            }
+            else
+            {
+                if (arr[leftChildIndex] <= arr[rightChildIndex])
+                {
+                    minIndex = leftChildIndex;
+                }
+                else
+                {
+                    minIndex = rightChildIndex;
+                }
+            }
+            if (arr[nodeIndex] > arr[minIndex])
+            {
+                tmp = arr[minIndex];
+
+                arr[minIndex] = arr[nodeIndex];
+
+                arr[nodeIndex] = tmp;
+
+                siftDown(minIndex);
+            }
+        }
+
+        private int GetRightChildIndex(int nodeIndex)
+        {
+            return (2 * nodeIndex) + 2;
+        }
+
+        private int GetLeftChildIndex(int nodeIndex)
+        {
+            return (2 * nodeIndex) + 1;
+        }
+
+        public void DisplayHeap()
+        {
+            Console.WriteLine("Elements of the heap:");
+            for (int i = 0; i < heapSize; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+
+            Console.WriteLine("\n***********************************");
+        }
+
+        public int GetMin()
+        {
+            return arr[0];
+        }
+
+        public void BuildMinHeap(int[] input)
+        {
+            if (heapSize > 0)
+            {
+                //clear the current heap
+                Array.Resize(ref arr, input.Length);
+                heapSize = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = input[i];
+                    heapSize++;
+                }
+            }
+            for (int i = heapSize - 1 / 2; i >= 0; i--)
+            {
+                MinHeapify(i);
+            }
+        }
+
+        private void MinHeapify(int index)
+        {
+            int left = 2 * index;
+            int right = (2 * index) + 1;
+            int smallest = index;
+            if (left < heapSize && arr[left] < arr[index])
+            {
+                smallest = left;
+            }
+            else
+            {
+                smallest = index;
+            }
+            if (right < heapSize && arr[right] < arr[smallest])
+            {
+                smallest = right;
+            }
+            if (smallest != index)
+            {
+                Swap(ref arr, index, smallest);
+                MinHeapify(smallest);
+            }
+        }
+
+        private void Swap(ref int[] input, int a, int b)
+        {
+            int temp = input[a];
+            input[a] = input[b];
+            input[b] = temp;
+        }
+
+        public void DeleteHeap()
+        {
+            Array.Resize(ref arr, 0);
+            heapSize = 0;
+        }
+    }
+}
---
+++ Stack101/Stack101/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Stack101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Stack stores the values in LIFO (Last in First out) style.
+             * The element which is added last will be the element to come out first.
+             */
+            var myStack = new Stack();
+            myStack.Push("Hello!!");
+            myStack.Push(null);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.Push(4);
+            myStack.Push(5);
+
+            foreach (var itm in myStack)
+                Console.Write(itm);
+
+            Console.WriteLine(myStack.Peek());
+            Console.WriteLine(myStack.Peek());
+            Console.WriteLine(myStack.Peek());
+
+            myStack.Contains(2); // returns true
+            myStack.Contains(10); // returns false
+
+            var myStack2 = new Stack();
+            myStack2.Push(1);
+            myStack2.Push(2);
+            myStack2.Push(3);
+            myStack2.Push(4);
+            myStack2.Push(5);
+
+            Console.Write("Number of elements in Stack: {0}", myStack2.Count);
+
+            while (myStack2.Count > 0)
+                Console.WriteLine(myStack2.Pop());
+
+            Console.Write("Number of elements in Stack: {0}", myStack2.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
