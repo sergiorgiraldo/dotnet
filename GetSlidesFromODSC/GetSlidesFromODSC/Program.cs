32dc85b4e8e571c8625a0074c2b57dbef3c236ad
Sergio Giraldo <sergiorgiraldo@gmail.com>
2019-05-21 16:36:51 -0300

    odsc scrapper

---
+++ GetSlidesFromODSC/GetSlidesFromODSC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GetSlidesFromODSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = new Regex("url=(https.*)", RegexOptions.Multiline);
+
+            DirectoryInfo d = new DirectoryInfo(@"C: \Users\sgiraldo\Downloads\");
+            foreach (var file in d.GetFiles())
+            {
+                using (StreamReader sr = new StreamReader(file.FullName))
+                {
+                    var contents = sr.ReadToEnd();
+
+                    var matches = pattern.Matches(contents);
+
+                    Console.WriteLine(matches[0].Groups[1].Value.Replace("\">", ""));
+                }
+            }
+            Console.Read();
+        }
+    }
+}
