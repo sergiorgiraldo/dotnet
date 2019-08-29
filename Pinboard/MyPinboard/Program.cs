51e0045ae1e927267a90da2a6330bbca15e0e057
Sergio Giraldo <sergio@giraldo.com.br>
2019-08-29 09:42:40 -0300

    pinboard command line utility

---
+++ Pinboard/MyPinboard/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using pinboard.net;
+
+namespace MyPinboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var onlyTags = false;
+            var filter = new List<string>();
+            if (args.Length > 0)
+            {
+                var parameters = args[0];
+                if (!string.IsNullOrEmpty(parameters))
+                {
+                    if (parameters == "-h" || parameters == "/?")
+                    {
+                        Console.WriteLine("mypinboard TAGS_SEPARATED_BY_COMMA");
+                        Console.WriteLine("mypinboard /tags");
+                        Console.WriteLine("mypinboard /t");
+                        Environment.Exit(0);
+                    }
+
+                    if (parameters == "/tags" || parameters == "/t")
+                    {
+                        onlyTags = true;
+                    }
+                    else
+                    {
+                        filter.AddRange(parameters.Split(","));
+                    }
+                        
+                }
+            }
+
+            //get your api token from https://pinboard.in/settings/password and store in a file elsewhere
+            string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string apiToken = File.ReadAllText(Path.Combine(userPath, "pinboard.key"));
+
+            using (var pb = new PinboardAPI(apiToken))
+            {
+                if (onlyTags)
+                {
+                    var tags = pb.Tags.Get().Result;
+
+                    foreach (var tag in tags)
+                    {
+                        Console.WriteLine(tag.Tag);
+                    }
+                }
+                else
+                {
+                    var posts = pb.Posts.All(filter).Result;
+
+                    foreach (var post in posts)
+                    {
+                        Console.WriteLine(post.Description + " -> "+ post.Href);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Pinboard/readme.md
@@ -0,0 +1,3 @@
+simple command line utility to retrieve tags or post from pinboard.
+
+it is ready to be used as a dotnet tool
