263c98c506f3d8376b55679b01507e64b16fc80d
Sergio Giraldo <sergio@giraldo.com.br>
2019-10-03 09:26:23 -0300

    reactive + service stack

---
+++ ServiceStack/reactive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reactive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
