2bfb8da19a7ced2fa652bd2d3c6c640aea8629d2
Sergio Giraldo <sergio@giraldo.com.br>
2019-11-05 09:57:13 -0300

    Updates by sgiraldo @ 20191105 09:57 (idea for implementing NEXT tuesday, NEXT monday, etc)

--- newMeeting/Program.cs
+++ newMeeting/Program.cs
@@ -74,6 +74,13 @@ namespace newMeeting
             else    
                 return what;
         }
+        //TODO
+        public static DateTime GetNextWeekday(this DateTime start, DayOfWeek day)
+        {
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int) day - (int) start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
 
         public static string GetApplicationRoot()
         {
