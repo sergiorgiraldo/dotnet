119167fc16c744f8596377ab4a843ceb79f498d4
Sergio Giraldo <sergio@giraldo.com.br>
2019-11-09 08:55:21 -0300

    Updates by sergio @ 20191109 08:55 (boilerplate for tool to shorten in command line)

---
+++ shorten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace shorten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //post to bitly
+
+            //insert into giral.do googl table
+            
+            //output shortened url
+
+            //copy to clipboard
+
+            Console.WriteLine("In construction");
+        }
+    }
+}
