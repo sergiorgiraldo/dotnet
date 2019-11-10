68b7b0dec80b37f1ec4dae027ad32693a4684157
Sergio Giraldo <sergio@giraldo.com.br>
2019-11-10 17:53:17 -0300

    Updates by sergio @ 20191110 17:53 (getting connection string and access key from static file)

--- shorten/Program.cs
+++ shorten/Program.cs
@@ -8,18 +8,28 @@ namespace shorten
 {
     class Program
     {
+        static string accessKey, connectionString;
         static void Main(string[] args)
         {
             if (args.Length == 0){
                 Console.WriteLine("shorten <url to be shortened>");
                 Environment.Exit(0);
             }
+            //Configuration
+            var config = File.ReadAllText(
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "shorten.config")
+                );
+            accessKey = config.Split('|')[0];
+            connectionString = config.Split('|')[1];
 
             //post to bitly
             var shortenedUrl = Shorten(args[0]).Replace("bit.ly", "s.giral.do");
 
+
+
             //insert into giral.do googl table
-            using(MySql.Simple.Database db = ""){
+            using(MySql.Simple.Database db = connectionString){
                 var sqlStatement = string.Format("INSERT INTO googl (url, short, myName) VALUES ('{0}','{1}','')",args[0], shortenedUrl);
                 db.Execute(sqlStatement);
 
@@ -32,7 +42,9 @@ namespace shorten
         public static string Shorten(string longUrl)
         {
             //get access token for bity in lastpass
-            var url = string.Format("https://api-ssl.bitly.com/v3/shorten?format=json&longUrl={0}&access_token=", longUrl);
+            var url = string.Format("https://api-ssl.bitly.com/v3/shorten?format=json&longUrl={0}&access_token={1}", 
+                longUrl,
+                accessKey);
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             try
