c2bea9bb55def5993f255a9286c5a616bb2b00fb
Sérgio Giraldo <sergio@giraldo.com.br>
2021-09-08 13:37:16 +0200

    Create helloWorldNetCore2.cs

---
+++ helloWorldNetCore2.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+RequestDelegate write = ctx => 
+    ctx.Response.WriteAsync("Hello world C# 9");
+
+Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(wb => 
+        wb.Configure(app => app.Run(write))
+    ).Build().Run();
+
